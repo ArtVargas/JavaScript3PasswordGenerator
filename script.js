7da5203aadb25633a62fbb264f55bc8b00b6fcdd
Arturo <arturodamianvargas1996@gmail.com>
2023-09-06 19:27:57 -0500

    added password function for password length

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment Code
+function generatePassword ()
+{
+
+  // Prompt user for password length
+  // Password Length must be between 8 and 128 characters
+  // Must include lowercase, uppercase. mumbers, special characters, symbols,
+  // If critera isn't keep prompting user until they enter a valid password
+
+    var passwordLength = parseInt(prompt("Password Length:"))
+      console.log(passwordLength);
+      if (passwordLength || passwordLength < 8 || passwordLength > 128) {
+      alert("Sorry! This is an invalid length. Please, try again")
+      return generatePassword ()
+      }
+
+  
+}
+
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
